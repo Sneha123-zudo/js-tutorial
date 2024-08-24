7d1eacd523cabba52560dbfdecd0ed7bb22c2b52
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-08-24 18:50:38 +0000

    done

--- 01_basics/StringOperation.js
+++ 01_basics/StringOperation.js
@@ -7,5 +7,33 @@ console.log(1+"2");
 console.log("1"+2+2);
 console.log(2+2+"1");
 
+let str1="Sneha "
+let str2="Mishra"
+let str3=str1+str2
+console.log(str3);
+
+//Increment
+
+let x = 4
+let w = 4
+let y = x++
+let z = ++w
+console.log(y);
+console.log(x);
+console.log(z);
+console.log(w)
+
+//Decrement
+
+let m = 10
+let n = m--
+console.log(n);
+console.log(m);
+let o = 10
+let p = --o;
+console.log(p);
+console.log(o);
+
+
 
 
