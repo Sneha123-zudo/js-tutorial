a2025ce28cab40c05600c0ef2231283cbc04ea12
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-08-27 08:02:50 +0000

    done

--- 01_basics/nums_maths.js
+++ 01_basics/nums_maths.js
@@ -9,6 +9,9 @@ console.log(balance.toString().length);
 console.log(balance.toFixed(2))
 const otherNumber = 23.8996;
 console.log(otherNumber.toPrecision(3));
+const Number2 = 24.343;
+console.log(Number2.toPrecision(3));
+
 const hundreds = 100000;
 console.log(hundreds.toLocaleString());
 
