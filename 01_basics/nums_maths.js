c6679c11750c355113fd4152f9fa4f349106bbb3
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-08-28 08:05:15 +0000

    num and math

--- 01_basics/nums_maths.js
+++ 01_basics/nums_maths.js
@@ -16,3 +16,24 @@ const hundreds = 100000;
 console.log(hundreds.toLocaleString());
 
 console.log(hundreds.toLocaleString('en-In'));
+
+
+//******************Maths*************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.2));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.76));
+console.log(Math.min(4,5,6,1,2));
+console.log(Math.max(42,768,10000,120000));
+
+
+console.log(Math.random());
+console.log((Math.random() * 10)+1)
+console.log(Math.floor(Math.random() * 10)+1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1))+ min)
