212786e53708429b715c309a3f2df4b90d235e8f
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-09-25 19:44:11 +0000

    object2

---
+++ 01_basics/object2.js
@@ -0,0 +1,32 @@
+// Singleton object creation
+ //const tinderUser = new Object();
+ // Literals object creation
+ const tinderUser={}
+    tinderUser.id ="123abc"
+    tinderUser.name = "Som"
+    tinderUser.email = "somgithub@gmail.com"
+//console.log(tinderUser)
+
+const realUser = {
+    email : "somgoogle@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname : "Hitesh",
+            lastname : "Choudhary"
+        }
+    }
+}
+console.log(realUser)
+console.log(realUser.fullname)
+console.log(realUser.fullname.userfullname)
+console.log(realUser.fullname.userfullname.firstname)
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {4:"e", 5:"f"}
+const obj3 = Object.assign(obj1,obj2,obj4)
+console.log(obj3)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
