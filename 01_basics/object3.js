e07ee4802cd24b39f8391ab1bc00c1a0d727e25c
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-09-26 15:22:12 +0000

    object or json and api

---
+++ 01_basics/object3.js
@@ -0,0 +1,29 @@
+//objects
+
+const course = {
+    name : "js in hindi",
+    price : "999Rs",
+    courseInstrutor : "Hitesh"
+}
+//console.log(course.courseInstrutor)
+
+const {courseInstrutor : instructor} = course
+console.log(instructor)
+
+//JSON and API
+
+// in object format
+
+//{
+//    "name" : "hitesh",
+//    "price" : "999rs",
+//    "date" : "12-july-24"
+//}
+
+// in array format
+
+[
+    {},
+    {},
+    {}
+]
