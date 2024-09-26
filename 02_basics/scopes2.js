7c4b2473e6e83791177490c46d1679c35e98f090
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-09-26 19:36:47 +0000

    scopes2

---
+++ 02_basics/scopes2.js
@@ -0,0 +1,22 @@
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+        console.log(username+website)
+    }
+    two()
+    //console.log(website)
+}
+one()
+
+
+if(true){
+    const username2 = "hitesh"
+    if(username2=="hitesh"){
+        const website = "instagram"
+        console.log(username2)
+    }
+   // console.log(website)
+}
+//console.log(username2)
