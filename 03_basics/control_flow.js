1db3cf7acac5865f0075ef4a7f4b9e5652b63f67
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-10-01 16:03:14 +0000

    if statement

---
+++ 03_basics/control_flow.js
@@ -0,0 +1,20 @@
+// if
+
+const age = 18
+if(age <= 20){
+    console.log("Age is less than 20")
+}
+else if(age == 18){
+    console.log("Age is equal to 18")
+}
+else if(age >= 20){
+    console.log("Age is not greater than 20")
+}
+else{
+    console.log("Age is not valid")
+}
+
+
+const balance = 1000
+if(balance > 500) console.log("balance is greater"), console.log("Balance is less");
+    
