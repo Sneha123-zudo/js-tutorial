698699c1b026f5c6f3365f73421006419d71c24a
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-10-02 12:02:28 +0000

    for loops

---
+++ 04_basics/forloops.js
@@ -0,0 +1,31 @@
+//for (let index = 0; index < array.length; index++) {
+//    const element = array[index];
+//    
+//}
+
+for (let index = 0; index < 10 ; index++) {
+    const element = index;
+    console.log(element);
+    
+    
+}
+
+//Table of n
+
+let n = 5
+console.log("Table of " + n);
+for (let index = 0; index <= 10; index++) {
+    console.log(n + "X" + index + "=" + n*index);
+    
+    
+}
+
+
+for (let i = 0; i <=10 ; i++) {
+    console.log(`Outer loop ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        console.log(`Inner loop ${i} and ${j}`)
+    }
+    
+}
