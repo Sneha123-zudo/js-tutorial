d1a55b16406b8f24b008f1489281f4aeeea87d0e
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-10-02 12:28:53 +0000

    for loops and break and continue

--- 04_basics/breakandcontinue.js
+++ 04_basics/breakandcontinue.js
@@ -2,9 +2,30 @@
 
 for (let index = 0; index < 10 ; index++) {
     const element = index;
-    console.log(element);
+    //console.log(element);
     if(index==5){
-        console.log(("It is 5"));
+        //console.log(("It is 5"));
         break;
     }
+}
+
+for (let index = 0; index < 10 ; index++) {
+    if(index==5){
+        //console.log(("It is 5"));
+        break;
+    }
+    const element = index;
+    console.log(element);
+}
+
+
+
+// continue statement
+for (let index = 0; index < 10 ; index++) {
+    if(index==5){
+        //console.log(("It is 5"));
+        continue;
+    }
+    const element = index;
+    console.log(element);
 }
--- 04_basics/forloops.js
+++ 04_basics/forloops.js
@@ -29,3 +29,9 @@ for (let i = 0; i <=10 ; i++) {
     }
     
 }
+const myArray = ["flash" , "superman" , "spiderman" , "hulk"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element)
+}
