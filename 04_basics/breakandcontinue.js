805176449263fb547f13633b1623730b0bbb18db
Sneha Mishra <145490348+Sneha123-zudo@users.noreply.github.com>
2024-10-02 12:07:15 +0000

    break

---
+++ 04_basics/breakandcontinue.js
@@ -0,0 +1,10 @@
+// break statement
+
+for (let index = 0; index < 10 ; index++) {
+    const element = index;
+    console.log(element);
+    if(index==5){
+        console.log(("It is 5"));
+        break;
+    }
+}
