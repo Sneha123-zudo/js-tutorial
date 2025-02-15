1724d2c426bed5c5a6865fec2a14cd4d5a89687d
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-02-15 14:26:26 +0530

    variable

---
+++ lec1/index.js
@@ -0,0 +1,42 @@
+// Globally scoped
+
+//var age = 25;
+//if(true){
+//    console.log(age);
+//}
+
+// Function scoped
+
+//function solve(){
+//    var age = 25;
+//    console.log(age);
+//}
+//solve();
+
+// Re definition is possible but not good
+
+//var a = 10;
+//var b = 20;
+
+//Will give error
+//{let a = 10;}
+//console.log(a)
+
+//Correct one 
+//{
+//    let age = 10;
+//    console.log(age)
+//}
+
+// Reassignment is possible
+
+let a = 10;
+console.log(a);
+
+a = "Sneha";
+console.log(a)
+
+// Redefinition is not possible
+
+//let b = 10;
+//let b = 20;
